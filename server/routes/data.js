1e951fff1ce1c29604ee913410b1ea9949b4c04f
duckheewon <fain9301@yahoo.com>
2017-12-28 11:01:57 +0900

    boarder router

---
+++ server/routes/data.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+
+//data index
+router.get('/', function(req, res, next) {
+
+});
+
+
+//get chart 
+router.get('/chart', function(req, res, next) {
+
+});
+
+//sequelize get data
+router.get('/ajax', function(req, res, next) {
+    var user_id = req.query.userid || req.params.userid;
+    var apikey = req.query.apikey || req.params.apikey;
+
+});
