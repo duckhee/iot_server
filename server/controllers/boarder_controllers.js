901c775ca9d44919bd0976c23a744ceac59b4785
duckheewon <fain9301@yahoo.com>
2018-01-10 13:50:26 +0900

    fix board controller

---
+++ server/controllers/boarder_controllers.js
@@ -0,0 +1,63 @@
+var models = requrie('../models/index.js');
+var board = require('../models/board');
+
+/**
+ *  boarder create and list, find, destory, search
+ * function
+ */
+
+exports.insert_board = function(board_info, callback) {
+    models.board.findOrCreate({}).then(function(row) {
+
+    }).catch(function(e) {
+
+    });
+};
+
+exports.list_board = function(board_info, callback) {
+    models.board.findAll({
+        where: {
+
+        }
+    }).then(function(rows) {
+
+    }).catch(function(e) {
+
+    });
+};
+
+exports.destory_board = function(board_info, callback) {
+    models.board.destroy({
+        where: {
+
+        }
+    }).then(function(row) {
+
+    }).catch(function(e) {
+
+    });
+};
+
+exports.search_board = function(board_info, callback) {
+    models.board.find({
+        where: {
+
+        }
+    }).then(function(row) {
+
+    }).catch(function(e) {
+
+    });
+};
+
+exports.search_list_board = function(board_info, callback) {
+    models.board.findAll({
+        where: {
+
+        }
+    }).then(function(rows) {
+
+    }).catch(function(e) {
+
+    });
+}
--- server/util/file.js
+++ server/util/file.js
@@ -240,12 +240,12 @@ exports.file_csv = function(file_info, callback) {
                     }
                 }
             }
+            var Num_i = 0;
             for (var i in row) {
                 //column width setting
                 //for문 하나
 
                 //cell에 number type이 들어가야해서 변환 
-                var Num_i = parseInt(i);
                 for (var c = 1; c <= 8; c++) {
                     if (c === 4) {
                         first_sheet.column(c).setWidth(44);
@@ -257,6 +257,94 @@ exports.file_csv = function(file_info, callback) {
                 //row height setting
                 //first_sheet.row(ii + 1).setHeight(10);
 
+                //date 넣어주기
+                //first_sheet.cell(Num_i + 2, 4).string(row[i].dataValues.createdAt.toString()).style(sheet_style);
+                var origin_date = new Date(row[i].dataValues.createdAt.toString());
+                var con_mindate = origin_date.getMinutes();
+                //var last_date;
+
+                //현재 데이터 시간 설정
+                if (con_mindate < 30) {
+                    origin_date.setMinutes(00);
+                    origin_date.setSeconds(00);
+                    //last_date = origin_date.getFullYear() + "-" + origin_date.getMonth() + "-" + origin_date.getDay() + " " + origin_date.getHours() + ':0' + origin_date.getMinutes();
+                } else {
+                    origin_date.setMinutes(30);
+                    origin_date.setSeconds(00);
+                    //last_date = origin_date.getFullYear() + "-" + origin_date.getMonth() + "-" + origin_date.getDay() + " " + origin_date.getHours() + ':' + origin_date.getMinutes();
+                }
+
+                //데이터가 30분 간격을 벗어나면 빈칸을 추가
+                if (i > 0) {
+                    //이전 데이터 설정
+                    var pre_date = new Date(row[i - 1].dataValues.createdAt.toString());
+                    con_mindate = pre_date.getMinutes();
+
+                    if (con_mindate < 30) {
+                        pre_date.setMinutes(00);
+                        pre_date.setSeconds(00);
+                        //last_date = origin_date.getFullYear() + "-" + origin_date.getMonth() + "-" + origin_date.getDay() + " " + origin_date.getHours() + ':0' + origin_date.getMinutes();
+                    } else {
+                        pre_date.setMinutes(30);
+                        pre_date.setSeconds(00);
+                        //last_date = origin_date.getFullYear() + "-" + origin_date.getMonth() + "-" + origin_date.getDay() + " " + origin_date.getHours() + ':' + origin_date.getMinutes();
+                    }
+
+                    var tmp = (pre_date.getTime() - origin_date.getTime()) / 60000; // 분 단위로 변경.
+
+                    //30분 간격보다 크면 데이터 손실이 있었던것으로 간주 그만큼 빈행을 넣어준다.
+                    if (tmp > 30) {
+                        for (var n = 1; n < tmp / 30; n++) {
+                            //번호 넣어주기
+                            first_sheet.cell(Num_i + 2, 1).number(Num_i + 1).style(sheet_style);
+
+                            //serial 넣어주기
+                            first_sheet.cell(Num_i + 2, 2).string(row[i].dataValues.sd_serial.toString()).style(sheet_style);
+
+                            //address 넣어주기
+                            first_sheet.cell(Num_i + 2, 3).string(row[i].dataValues.sd_address.toString()).style(sheet_style);
+
+                            first_sheet.cell(Num_i + 2, 4).date(pre_date.setTime(pre_date.getTime() - (30 * 60 * 1000))).style({
+                                alignment: {
+                                    horizontal: ['center'],
+                                    vertical: ['center']
+                                },
+                                font: {
+                                    size: 10,
+                                    bold: false,
+                                },
+                                border: {
+                                    left: {
+                                        style: 'thin',
+                                        color: '#000000'
+                                    },
+                                    right: {
+                                        style: 'thin',
+                                        color: '#000000'
+                                    },
+                                    top: {
+                                        style: 'thin',
+                                        color: '#000000'
+                                    },
+                                    bottom: {
+                                        style: 'thin',
+                                        color: '#000000'
+                                    }
+                                },
+                                numberFormat: 'yyyy-mm-dd hh:mm'
+                            });
+
+                            for (var a = 0; a < 24; a++) {
+                                //write data
+                                first_sheet.cell(Num_i + 2, Num_a + 5).string("-").style(sheet_style);
+                            }
+
+                            Num_i++;
+                        }
+
+                    }
+                }
+
                 ////////////////for 문 제어 필요///////
                 //번호 넣어주기
                 first_sheet.cell(Num_i + 2, 1).number(Num_i + 1).style(sheet_style);
@@ -267,8 +355,37 @@ exports.file_csv = function(file_info, callback) {
                 //address 넣어주기
                 first_sheet.cell(Num_i + 2, 3).string(row[i].dataValues.sd_address.toString()).style(sheet_style);
 
+                //first_sheet.cell(Num_i + 2, 4).string(last_date).style(sheet_style);
                 //date 넣어주기
-                first_sheet.cell(Num_i + 2, 4).string(row[i].dataValues.createdAt.toString()).style(sheet_style);
+                first_sheet.cell(Num_i + 2, 4).date(origin_date).style({
+                    alignment: {
+                        horizontal: ['center'],
+                        vertical: ['center']
+                    },
+                    font: {
+                        size: 10,
+                        bold: false,
+                    },
+                    border: {
+                        left: {
+                            style: 'thin',
+                            color: '#000000'
+                        },
+                        right: {
+                            style: 'thin',
+                            color: '#000000'
+                        },
+                        top: {
+                            style: 'thin',
+                            color: '#000000'
+                        },
+                        bottom: {
+                            style: 'thin',
+                            color: '#000000'
+                        }
+                    },
+                    numberFormat: 'yyyy-mm-dd hh:mm'
+                });
                 //data parser
                 var array = row[i].dataValues.sd_data.split(',');
                 //write data 
@@ -283,6 +400,8 @@ exports.file_csv = function(file_info, callback) {
                 }
 
                 //두번째 for문 끝
+
+                Num_i++;
             }
 
             ////////////여기 까지 ////////////////
@@ -290,7 +409,8 @@ exports.file_csv = function(file_info, callback) {
             console.log('success ! ');
 
             //make csv file or download file 
-            csvfile.write('download_date ' + date + '.csv', file_info.response);
+            csvfile.write(file_info.sd_serial + '_download_date ' + date + '.csv', file_info.response);
+            //csvfile.write(file_info.sd_serial + '_download_date ' + date + '.xlsl', file_info.response);
             //만약 서버에 파일을 생성을 하고 싶으면 밑에 주석 풀고 위에 주석 처리
             //csvfile.write('download_date'+date+'.csv);
             callback(row, null);
@@ -316,3 +436,132 @@ exports.get_filesize = function(file_info, callback) {
         callback(null, err);
     }
 };
+
+
+
+//file_info에 sd_serial 정보가 들어가야한다.
+//make data file .csv callback error file_info에 res를 담아주어야한다.
+/*
+exports.data_one_csv = function(file_info, callback) {
+        var csvfile = new excel.Workbook();
+        //first sheet add file
+        var first_sheet = csvfile.addWorksheet('sheet1');
+        //sheet style set up
+        var sheet_style = csvfile.createStyle({
+            alignment: {
+                horizontal: ['center'],
+                vertical: ['center']
+            },
+            font: {
+                size: 10,
+                bold: false,
+            },
+            border: {
+                left: {
+                    style: 'thin',
+                    color: '#000000'
+                },
+                right: {
+                    style: 'thin',
+                    color: '#000000'
+                },
+                top: {
+                    style: 'thin',
+                    color: '#000000'
+                },
+                bottom: {
+                    style: 'thin',
+                    color: '#000000'
+                }
+            }
+        });
+        
+        //    column A : index
+        //    column B : serial 
+        //    column C : address
+        //    column D : date
+        //    column E ~  : value
+        //    serial, address, value, date, index
+        
+        
+            //data 모두 가져오기 
+            valueControllers.download_data(file_info, function(row, err) {
+                if (row) {
+                    //setting title
+                    first_sheet.cell(1, 1).string("no").style(sheet_style);
+                    first_sheet.cell(1, 2).string("serial").style(sheet_style);
+                    first_sheet.cell(1, 3).string("address").style(sheet_style);
+                    first_sheet.cell(1, 4).string("date").style(sheet_style);
+                    var testing = row[1].dataValues.sd_data.split(',');
+                    //value title setting
+                    for (var j = 1; j <= testing.length; j++) {
+                        if (j <= 8) {
+                            //처음 명령어가 ec 정보 가져오기
+                            for (var number = 1; number <= 8; number++) {
+                                first_sheet.cell(1, 4 + j).string("ec" + number).style(sheet_style);
+                            }
+                        } else if (j <= 16) {
+                            //두번째 명령어가 온도 정보 가져오기
+                            for (var number = 1; number <= 8; number++) {
+                                first_sheet.cell(1, 4 + j).string("온도" + number).style(sheet_style);
+                            }
+                        } else if (j <= 24) {
+                            //세번째 명령어가 수분 정보 가져오기
+                            for (var number = 1; number <= 8; number++) {
+                                first_sheet.cell(1, 4 + j).string("수분" + number).style(sheet_style);
+                            }
+                        }
+                    }
+                    for (var i in row) {
+                        //column width setting
+                        //for문 하나
+                        //cell에 number type이 들어가야해서 변환 
+                        var Num_i = parseInt(i);
+                        for (var c = 1; c <= 8; c++) {
+                            if (c === 4) {
+                                first_sheet.column(c).setWidth(44);
+                            } else {
+                                first_sheet.column(c).setWidth(20);
+                            }
+                        }
+                        //for문 끝
+                        //row height setting
+                        //first_sheet.row(ii + 1).setHeight(10);
+                        ////////////////for 문 제어 필요///////
+                        //번호 넣어주기
+                        first_sheet.cell(Num_i + 2, 1).number(Num_i + 1).style(sheet_style);
+                        //serial 넣어주기
+                        first_sheet.cell(Num_i + 2, 2).string(row[i].dataValues.sd_serial.toString()).style(sheet_style);
+                        //address 넣어주기
+                        first_sheet.cell(Num_i + 2, 3).string(row[i].dataValues.sd_address.toString()).style(sheet_style);
+                        //date 넣어주기
+                        first_sheet.cell(Num_i + 2, 4).string(row[i].dataValues.createdAt.toString()).style(sheet_style);
+                        //data parser
+                        var array = row[i].dataValues.sd_data.split(',');
+                        //write data 
+                        //여기서 부터 for문 하나
+                        for (var a in array) {
+                            //string을 number type으로 변환 
+                            var Num_a = parseInt(a);
+                            //write data
+                            first_sheet.cell(Num_i + 2, Num_a + 5).string(array[a].toString()).style(sheet_style);
+                        }
+                        //두번째 for문 끝
+                    }
+                    ////////////여기 까지 ////////////////
+                    var date = moment().format('YYYYMMDD');
+                    console.log('success ! ');
+                    //make csv file or download file 
+                    csvfile.write('download_date ' + date + '.csv', file_info.response);
+                    //만약 서버에 파일을 생성을 하고 싶으면 밑에 주석 풀고 위에 주석 처리
+                    //csvfile.write('download_date'+date+'.csv);
+                    callback(row, null);
+                } else if (err) {
+                    console.log('error : ', err.stack);
+                    callback(null, err);
+                } else {
+                    callback(null, null);
+                }
+            });
+        };
+        */
