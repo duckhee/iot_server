0259c6f73f64957f189c5617e57db6017b7a6284
duckheewon <fain9301@yahoo.com>
2018-01-10 13:32:25 +0900

    add router boarder

--- server/routes/boarder.js
+++ server/routes/boarder.js
@@ -1,2 +1,19 @@
 var express = require('express');
 var router = express.Router();
+
+
+router.get('/', functino(req, res, next) {
+
+});
+
+router.get('/registe', function(req, res, next) {
+
+});
+
+router.get('/list', function(req, res, next) {
+
+});
+
+router.get('/detail', function(req, res, next) {
+
+});
