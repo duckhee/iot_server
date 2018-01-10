d93f33807552998b1ed12c34592f72af5e9a48c9
duckheewon <fain9301@yahoo.com>
2018-01-10 11:49:45 +0900

    fix user datatype

--- server/migrations/20171228021024-create-user.js
+++ server/migrations/20171228021024-create-user.js
@@ -1,54 +1,57 @@
 'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userid: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      userage: {
-        type: Sequelize.INTEGER
-      },
-      user_address1: {
-        type: Sequelize.STRING
-      },
-      user_address2: {
-        type: Sequelize.STRING
-      },
-      zip_code: {
-        type: Sequelize.STRING
-      },
-      user_phone1: {
-        type: Sequelize.INTEGER
-      },
-      usre_phone2: {
-        type: Sequelize.INTEGER
-      },
-      user_phone3: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('users');
-  }
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userid: {
+                type: Sequelize.STRING,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING
+            },
+            username: {
+                type: Sequelize.STRING
+            },
+            userage: {
+                type: Sequelize.INTEGER
+            },
+            user_address1: {
+                type: Sequelize.STRING
+            },
+            user_address2: {
+                type: Sequelize.STRING
+            },
+            zip_code: {
+                type: Sequelize.STRING
+            },
+            user_phone1: {
+                type: Sequelize.INTEGER
+            },
+            usre_phone2: {
+                type: Sequelize.INTEGER
+            },
+            user_phone3: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: 'TIMESTAMP',
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: 'TIMESTAMP',
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('users');
+    }
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,22 +1,25 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var user = sequelize.define('user', {
-    userid: DataTypes.STRING,
-    password: DataTypes.STRING,
-    username: DataTypes.STRING,
-    userage: DataTypes.INTEGER,
-    user_address1: DataTypes.STRING,
-    user_address2: DataTypes.STRING,
-    zip_code: DataTypes.STRING,
-    user_phone1: DataTypes.INTEGER,
-    usre_phone2: DataTypes.INTEGER,
-    user_phone3: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return user;
+    var user = sequelize.define('user', {
+        userid: {
+            type: DataTypes.STRING,
+            unique: true,
+        },
+        password: DataTypes.STRING,
+        username: DataTypes.STRING,
+        userage: DataTypes.INTEGER,
+        user_address1: DataTypes.STRING,
+        user_address2: DataTypes.STRING,
+        zip_code: DataTypes.STRING,
+        user_phone1: DataTypes.INTEGER,
+        usre_phone2: DataTypes.INTEGER,
+        user_phone3: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return user;
 };
