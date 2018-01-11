6a242af185f6d94bfb26d1b0ba45a56f95ced67a
duckheewon <fain9301@yahoo.com>
2018-01-11 15:42:39 +0900

    add user router

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,5 +6,37 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
+router.get('/registe', function(req, res, next) {
+
+});
+
+router.post('/registe', function(req, res, next) {
+
+});
+
+router.get('/profile', function(req, res, next) {
+
+});
+
+router.post('/profile', function(req, res, next) {
+
+});
+
+router.get('/modify', function(req, res, next) {
+
+});
+
+router.post('/modify', function(req, res, next) {
+
+});
+
+router.get('/delete', function(req, res, next) {
+
+});
+
+router.post('/delete', function(req, res, next) {
+
+});
+
 
 module.exports = router;
