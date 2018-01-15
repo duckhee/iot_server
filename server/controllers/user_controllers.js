ff715b6189df0180e70785107a2e7380f9875d0a
duckheewon <fain9301@yahoo.com>
2018-01-15 09:47:49 +0900

    fix user controllers

--- server/controllers/user_controllers.js
+++ server/controllers/user_controllers.js
@@ -11,10 +11,18 @@ var users = require('../models/user');
 exports.create_user = function(user_info, callback) {
     models.users.findOrCreate({
         where: {
-
+            userid: user_info.userid
         },
         defaults: {
-
+            password: user_info.password,
+            username: user_info.username,
+            userage: user_info.userage,
+            user_address1: user_info.user_address1,
+            user_address2: user_info.user_address2,
+            zip_code: user_info.zip_code,
+            user_phone1: user_info.user_phone1,
+            user_phone2: user_info.user_phone2,
+            user_phone3: user_info.user_phone3,
         }
     }).then(function(row) {
         console.log('success');
@@ -28,7 +36,7 @@ exports.create_user = function(user_info, callback) {
 exports.select_one_user = function(user_info, callback) {
     models.users.find({
         where: {
-
+            userid: user_info.userid
         }
     }).then(function(row) {
         console.log('success');
@@ -56,7 +64,9 @@ exports.select_list_user = function(user_info, callback) {
 //modfiy user
 exports.modfiy_user = function(user_info, callback) {
     models.users.update({}, {
-        where: {}
+        where: {
+            userid: user_info.userid
+        }
     }).then(function(row) {
         console.log('success');
         callback(row, null);
@@ -69,7 +79,7 @@ exports.modfiy_user = function(user_info, callback) {
 exports.destory_user = function(user_info, callback) {
     models.usrs.destroy({
         where: {
-
+            userid: user_info.userid
         }
     }).then(function(row) {
         console.log('success');
