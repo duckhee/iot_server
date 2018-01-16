e0b1063d70c549bfc836ae92aca80cd3aca6d6f7
duckheewon <fain9301@yahoo.com>
2018-01-16 14:22:39 +0900

    ajax router add

--- server/routes/data.js
+++ server/routes/data.js
@@ -28,37 +28,5 @@ router.get('/image', function(req, res, next) {
 
 });
 
-//sequelize get 
-router.get('/ajax', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
-    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
-
-});
-
-router.post('/ajax', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
-    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
-
-});
-//sequelize get data
-router.get('/ajaxdata', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
-    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
-});
-
-router.post('/ajaxdata', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
-    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
-});
-//sequelize get images
-router.get('/ajaximages', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
-    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
-});
-
-router.post('/ajaximages', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
-    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
-});
 
 module.exports = router;
