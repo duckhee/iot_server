b5a18e82fc6c4b66baca1437e882337ca67ef1ee
duckheewon <fain9301@yahoo.com>
2018-01-16 14:04:24 +0900

    add data router

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,30 +1,64 @@
 var express = require('express');
 var router = express.Router();
 var bCrypt = require('bcrypt-nodejs');
-
+var data_controller = require('../controllers/data_controllers');
 
 //data index
 router.get('/', function(req, res, next) {
 
 });
 
+//show images and data
+router.get('/show', function(req, res, next) {
+
+});
 
 //get chart 
 router.get('/chart', function(req, res, next) {
 
 });
 
-//sequelize get data
-router.get('/ajax', function(req, res, next) {
-    var user_id = req.query.userid || req.params.userid;
-    var apikey = req.query.apikey || req.params.apikey;
+//only show data
+router.get('/data', function(req, res, next) {
 
 });
 
-router.get('/ajax', function(req, res, next) {
-    var user_id = req.query.user_id || req.params.user_id;
-    var apikey = req.query.apikey || req.params.apikey;
+//only show image
+router.get('/image', function(req, res, next) {
 
+});
+
+//sequelize get 
+router.get('/ajax', function(req, res, next) {
+    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
+    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
+
+});
+
+router.post('/ajax', function(req, res, next) {
+    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
+    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
+
+});
+//sequelize get data
+router.get('/ajaxdata', function(req, res, next) {
+    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
+    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
+});
+
+router.post('/ajaxdata', function(req, res, next) {
+    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
+    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
+});
+//sequelize get images
+router.get('/ajaximages', function(req, res, next) {
+    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
+    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
+});
+
+router.post('/ajaximages', function(req, res, next) {
+    var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
+    var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
 });
 
 module.exports = router;
