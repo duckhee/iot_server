1f4a3bb5902d36067a5b75c795d0862f9d88d4ed
duckheewon <fain9301@yahoo.com>
2018-01-17 17:02:00 +0900

    add community router

---
+++ server/routes/community_router.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+});
+
+router.get('/list', function(req, res, next) {
+
+});
+
+router.get('/registe', function(req, res, next) {
+
+});
+
+
+
+module.exports = router;
