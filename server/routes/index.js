7c5aeca85612a7b231e7792507db846846a61981
duckhee <fain9301@yahoo.com>
2018-01-19 14:00:19 +0900

    win32 add start index
    start index page add

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+router.get('/test_index', function(req, res, next){
+    res.render('start_index');
+});
+
 module.exports = router;
