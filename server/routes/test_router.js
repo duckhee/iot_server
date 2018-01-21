93c41f004aa6e7503ee2ec62a5c85423413c290d
duckhee <fain9301@yahoo.com>
2018-01-21 21:32:15 +0900

    testing 1

---
+++ server/routes/test_router.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/test1', function(req, res, next) {
+    res.render('start_index');
+});
+
+router.get('/test2', function(req, res, next) {
+    res.render('test_index');
+});
+router.get('/test3', function(req, res, next) {
+    res.render('testing_index');
+});
+
+
+module.exports = router;
