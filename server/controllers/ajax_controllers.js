f85990ab59ae26acf70f7e7fe0631b82f6cc680b
duckhee <fain9301@yahoo.com>
2018-01-22 18:14:51 +0900

    win32 ajax controller add
     ajax controller add

---
+++ server/controllers/ajax_controllers.js
@@ -0,0 +1,40 @@
+
+
+var models = require('../models/index');
+var device_data = require('../models/device_data');
+
+
+//ajax get limit data
+exports.ajax_getlistdata = function(ajax_info, callback){
+    models.device_data.findAll({
+        where:{
+            data_apikey:ajax_info.apikey
+        },
+        limit:10
+    }).then(function(row){
+
+    }).catch(function(err){
+
+    });
+};
+//ajax get all data
+exports.ajax_getalldata = function(ajax_info, callback){
+    models.device_data.findAll({
+        where:{
+            data_apikey:ajax_info.apikey
+        }
+    }).then(function(rows){
+
+    }).catch(function(err){
+
+    });
+};
+//ajax get image path
+exports.ajax_image = function(ajax_info, callback){
+
+};
+
+//ajax get image all 
+exports.ajax_imagelist = funciton(ajax_info, callback){
+
+}
--- server/routes/ajax_router.js
+++ server/routes/ajax_router.js
@@ -8,6 +8,7 @@ var device_controllers = require('../controllers/device_controller');
 router.get('/ajax', function(req, res, next) {
     var user_id = req.query.user_id || req.params.user_id || req.body.user_id;
     var apikey = req.query.apikey || req.params.apikey || req.body.apikey;
+    
 
 });
 
